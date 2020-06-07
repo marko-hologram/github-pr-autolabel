eb66b6ab374636faab604fc1e7fd1a0f5f13d8d1
Marko Antolić <marko.antolic01@gmail.com>
2020-06-08 00:28:50 +0200

    Add user settings and better error handling

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -1,9 +1,21 @@
+import "toastify-js/src/toastify.css";
+import "~/src/style/style-content.scss";
 import { MessageType } from "~/src/constants";
-import { setPRLabels, TSingleEntry } from "~/src/utility";
+import { setPRLabels, TSingleEntry, getEmphasizedLabels } from "~/src/utility";
+import { showToast } from "~/src/toasts";
 
-chrome.runtime.onMessage.addListener(function (request) {
+chrome.runtime.onMessage.addListener(async (request) => {
   if (request && request.type === MessageType.MATCHED_PR_PAGE_OPENED) {
     const entryData: TSingleEntry = request.entryData;
-    setPRLabels(entryData.labels);
+
+    try {
+      await setPRLabels(entryData.labels);
+    } catch (error) {
+      showToast.error(
+        `There was an error while trying to set labels for this PR. Please set them manually.<br /> Labels you were trying to set were: "${getEmphasizedLabels(
+          entryData.labels
+        )}".`
+      );
+    }
   }
 });

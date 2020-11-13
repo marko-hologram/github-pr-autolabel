abb0875c31ed1bd971705445d3c6673f88df039d
Marko Antolić <marko@borealis.biz>
2020-11-13 13:14:45 +0100

    Add intentional eslint errors

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import classnames from "classnames";
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
